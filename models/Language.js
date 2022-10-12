fdbe6d398bb15b75f01cbe57efba89abd6267335
Talee Y <talee.yng@gmail.com>
2022-10-12 18:41:12 -0500

    Add models and seeds

---
+++ models/Language.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Language extends Model {}
+
+Language.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'language'
+    }
+);
+
+module.exports = Language;
---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+const Language = require('./Language');
+const Word = require('./Word');
+
+module.exports = {
+    User,
+    Language,
+    Word
+};
