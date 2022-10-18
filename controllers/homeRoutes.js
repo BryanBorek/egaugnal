6df4868c8f784927082167197d0649fa20aa90cf
Bryan Borek <bryantborek@gmail.com>
2022-10-17 21:52:07 -0500

    pushing up my changes from today mostly language card and handlebars and language model

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,62 +5,46 @@ let selectedLanguage = {};
 const googleTTS = require('google-tts-api');
 const translate = require('@vitalets/google-translate-api');
 
-let chosenLanguage = "";
-
 router.get("/", async (req, res) => {
   res.render("homepage", {
     loggedIn: req.session.loggedIn,
   })
 });
-
 router.get("/login", (req, res) => {
   if (!req.session.loggedIn) {
     res.redirect("/");
     return;
   }
-
   res.redirect("/startpage");
 });
-
-
-
 router.get("/startpage", withAuth, async (req, res) => {
   const language = await Language.findAll(
     { raw: true, }
   )
-
   res.render("startpage", {
     language,
   });
 });
-
 router.get("/learningpage/languageId/:languageId/wordIndex/:wordIndex", async (req, res) => {
   const languageId = parseInt(req.params.languageId);
   const wordIndex = parseInt(req.params.wordIndex);
-
   const selectedLanguage = await Language.findByPk(languageId, {
     raw: true,
   });
-
   //find all words in database and count them
   const wordResponse = await Word.findAndCountAll({
     raw: true,
     offset: wordIndex,
     limit: 1,
   })
-
   const displayWord = wordResponse.rows[0]
-
   //Do not show Next button if there are no more words
   let nextBtnURL;
-
   const nextIndex = wordIndex + 1;
   const maximumIndex = wordResponse.count - 1;
-
   // Check if there are still words in the database
   if (nextIndex <= maximumIndex) {
     nextBtnURL = `/learningpage/languageId/${languageId}/wordIndex/${wordIndex + 1}`;
-
   }
   //Transforms the english word to the desired language
   const transformWord = await translate(displayWord.word_name, { to: selectedLanguage.short });
@@ -74,7 +58,6 @@ router.get("/learningpage/languageId/:languageId/wordIndex/:wordIndex", async (r
   })
   //Create the audio
   const audioSource = `data:audio/wav;base64,${audioBase64}`;
-
   res.render("learningpage", {
     displayWord,
     foreignWord,
@@ -85,9 +68,7 @@ router.get("/learningpage/languageId/:languageId/wordIndex/:wordIndex", async (r
     nextBtnURL,
   })
 })
-
 router.get("/logout", async (req, res) => {
   res.render("homepage")
 });
-
 module.exports = router;
